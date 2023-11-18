22b1ed8e5e4da7d7d5e61668392098fb3dd86565
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-11-17 21:32:48 -0300

    Add files via upload

---
+++ Ad-Hoc/Beecrowd/3174.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define no_tle_pls cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define f first
+#define s second
+#define debug(x) cout << #x << " is " << x << '\n'
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fl;
+const int MAX = 1e9 + 7;
+
+map <char, int> hrs {{'b', 8}, {'a', 4}, {'m', 6}, {'d', 12}};
+map <char, int> rest {{'b', 0}, {'a', 0}, {'m', 0}, {'d', 0}};
+
+void solve() {
+    int t, ans {}; cin >> t;
+    while (t--) {
+        string n, p;
+        int cghr;
+        cin >> n >> p >> cghr;
+        ans += cghr / hrs[p[0]];
+        rest[p[0]] += cghr % hrs[p[0]];
+        ans += rest[p[0]] / hrs[p[0]];
+        if (rest[p[0]] >= hrs[p[0]]) rest[p[0]] -= hrs[p[0]];
+    }
+    cout << ans << '\n';
+}
+
+int main() { no_tle_pls
+    solve();
+    return 0;
+}
+
+
+
+
+
+
+
+
