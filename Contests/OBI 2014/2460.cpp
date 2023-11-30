5dd26f41a92f24d0fd13669320194b0675d76758
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-11-30 10:05:50 -0300

    Update 2460.cpp

--- Contests/OBI 2014/2460.cpp
+++ Contests/OBI 2014/2460.cpp
@@ -1,27 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define no_tle_pls cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+#define no_tle_pls ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define all(x) (x).begin(), (x).end()
 #define pb push_back
-#define rvb pop_back
-#define pf push_front
-#define rvf pop_front
-#define lb lower_bound
-#define up upper_bound
 #define f first
 #define s second
-#define q '\n'
-#define sz(x) (int)(x).size()
-#define debug(x) cout << #x << " is " << x << q
+#define debug(x) cout << #x << " is " << x << '\n'
 using ll = long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using vi = vector<int>;
-using vll = vector<ll>;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fl;
-const int MAX = 2 << 17;
+const int MAX = 2 << 17
 
 int main() { no_tle_pls
     int n; cin >> n;
