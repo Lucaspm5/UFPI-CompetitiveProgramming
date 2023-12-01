d46296f1cd09f5b55aa10426e01e1ed80324ac28
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-12-01 15:23:38 -0300

    Update 3145.cpp

--- Mathematics/Beecrowd/3145.cpp
+++ Mathematics/Beecrowd/3145.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define no_tle_pls ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define f first
+#define s second
+#define debug(x) cout << #x << " is " << x << '\n'
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fl;
+const int MAX = 2 << 17;
+
+int main() {
+    int n, x; cin >> n >> x;
+    double ans =  (double)x / (n + 2);
+    printf("%.2lf\n", ans);
+    return 0;
+}
 
