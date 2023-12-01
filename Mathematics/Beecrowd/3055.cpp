fb7f5262d1a328c6e6d6e291b44ef83a0973cd9b
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-12-01 15:12:35 -0300

    Create 3055.cpp

---
+++ Mathematics/Beecrowd/3055.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define no_tle_pls ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define f first
+#define s second
+#define debug(x) cout << #x << " is " << x << '\n'
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fl;
+const int MAX = 2 << 17;
+
+int main() {
+    int n, m; cin >> n >> m;
+    cout << ((m << 1) - n) << '\n';
+    return 0;
+}
