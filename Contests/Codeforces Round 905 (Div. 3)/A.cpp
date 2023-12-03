be2d8c389a181b2835c6fb3d2ed74608233dba36
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-12-03 10:32:23 -0300

    Update A.cpp

--- Contests/Codeforces Round 905 (Div. 3)/A.cpp
+++ Contests/Codeforces Round 905 (Div. 3)/A.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define no_tle_pls ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define all(x) (x).begin(), (x).end()
 #define pb push_back
@@ -10,9 +10,7 @@ using namespace std;
 using ll = long long;
 using vi = vector<int>;
 using pii = pair<int, int>;
-
-vi v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+ 
 int main() { no_tle_pls
     size_t t; cin >> t;
     cin.ignore(1, '\n');
@@ -24,8 +22,8 @@ int main() { no_tle_pls
         int pos = 0;
         for (size_t i = 0;i < 4;++i) {
             int x = s[i] == '0' ? 10 : s[i] - '0';
-            int it = lower_bound(all(v), x) - v.begin();
-            ans += 1 + (abs(it - pos));
+            int it = x - 1;
+            ans += 1 + abs(it - pos);
             pos = it;
         }
         cout << ans << '\n';
