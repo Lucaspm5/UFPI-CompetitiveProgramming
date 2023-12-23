3c3c8b95aab710e75a4bff3158057ddbfe65b250
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-12-23 07:48:44 -0300

    Create Increasing Array.cpp

---
+++ CSES/Introductory Problems/Increasing Array.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define no_tle_pls cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define rvb pop_back
+#define pf push_front
+#define rvf pop_front
+#define lb lower_bound
+#define up upper_bound
+#define f first
+#define s second
+#define q '\n'
+#define sz(x) (int)(x).size()
+#define debug(x) cout << #x << " is " << x << q
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fl;
+const int MAX = 1e9 + 7;
+ 
+int main() { no_tle_pls
+    int n; cin >> n;
+    vll v(n);
+    for (ll &x : v) cin >> x;
+    ll ans = 0;
+    for (int i = 1;i < n;i++) {
+        if (v[i] < v[i-1]) {
+            ans += v[i-1] - v[i];
+            v[i] += v[i-1] - v[i];
+        }
+    }
+    cout << ans << q;
+    return 0;
+}
