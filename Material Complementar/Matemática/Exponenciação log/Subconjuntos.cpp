50038fae9fd6a878c531be6a2d4fe904f893cedf
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-12-24 14:16:38 -0300

    Update Subconjuntos.cpp

--- Material Complementar/Matemática/Exponenciação log/Subconjuntos.cpp
+++ Material Complementar/Matemática/Exponenciação log/Subconjuntos.cpp
@@ -17,6 +17,7 @@ int main() {
     vi x {1, 2, 3};
     // i = 0, conjunto vazio
     // 1 << n, congruente a 2^n
+    // 1 << n, numero de conjuntos que podem ser formados em n elementos
     for (int i = 0;i < (1 << 3);++i) {
         for (int j = 0;j < 3;++j) {
             // verifica se o bit os j-th bit estão ligados ou não, caso ligagos imprimi o j-th elemento
