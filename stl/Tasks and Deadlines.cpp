3c7cc69e2262d2e31ef5241ee7fddaf6922ed527
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2024-06-10 15:29:18 -0300

    Create Tasks and Deadlines.cpp

---
+++ stl/Tasks and Deadlines.cpp
@@ -0,0 +1,51 @@
+//https://cses.fi/problemset/result/9507043/
+#define LOCAL
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define pf push_front
+#define f first
+#define s second
+#define v(i) vector<i>
+#define p(i, j) pair<i, j>
+#define m(i, j) map<i, j>
+#define lb lower_bound
+#define up upper_bound
+#define MAX INT32_MAX
+#define MIN INT32_MIN
+#define FOR(i, a, b) for(int i = a; i < (b); ++i)
+#define _for(a, x) for(auto& a : x)
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using ll = long long;
+
+int n;
+
+int main() { _
+    cin >> n;
+    priority_queue<p(int, int), v(p(int, int)), greater<p(int, int)>> pq;
+    FOR(i, 0 ,n) {
+        int x, y; cin >> x >> y;
+        pq.push({x, y});
+    }
+    ll curr_time = 0, ans = 0;
+    while(sz(pq)) {
+        curr_time += pq.top().f;
+        ans += (pq.top().s - curr_time);
+        pq.pop();
+    }
+    cout << ans << '\n';
+    return 0;
+}
