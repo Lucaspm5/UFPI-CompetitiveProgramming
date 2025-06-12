d31a7d29237ceb629ff0fedf6d40fe4bcb598b1a
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2025-06-12 18:13:29 -0300

    Create tock Buy and Sell – Multiple Transaction Allowed.cpp

---
+++ Ad-hoc/tock Buy and Sell – Multiple Transaction Allowed.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    
+    int maximumProfit(vector<int> &prices) {
+            
+            prices.push_back(-1);
+            int ans = 0, v = prices.front(), p = prices.front();
+            
+            for (int i = 1;i < prices.size();++i) {
+            
+            	if (prices[i] < prices[i - 1]) {
+            	    
+            	    if (p != -1) {
+            	        ans += (p - v);
+            	        p = -1;
+            	    }
+            	        
+            		v = prices[i];
+            	} else p = prices[i];
+            	
+            	
+            }
+            
+            return ans;
+    }
+};
